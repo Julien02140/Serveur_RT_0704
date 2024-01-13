9ed127e32b9dcb3815e406e97bdf5e453244de23
Julien Boutreaux <julien.boutreaux@orange.fr>
2024-01-13 17:57:15 +0100

    essai

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
-    return "hello world local windows"
+    return render_template('index.html')
+
+@helloworld.route("/login")
+def login():
+    return render_template('login.html')
+
+@helloworld.route("/register")
+def register():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0",port=int("3000"),debug=True)
