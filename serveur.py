ecf472e661bf2fdc86ab340218b5a8b10f304949
toto <julien.boutreaux@orange.fr>
2024-01-27 15:29:23 +0000

    Authentification avec flask_login, page admin crée

--- serveur.py
+++ serveur.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify, session, url_for, redirect,g, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import requests
 import os
 
@@ -6,7 +7,42 @@ import os
 API_URL = "http://api:5000/"
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.config['SECRET_KEY'] = os.urandom(24)
+login_manager = LoginManager(app)
+
+#modèle utilisateur
+class User(UserMixin):
+    def __init__(self, user_id,name,is_admin):
+        self.id = user_id
+        self.name = name
+        self.is_admin = is_admin
+    
+    def get_id(self):
+        return str(self.id)
+
+#admin_user = User(0,"admin")
+
+# Fonction de chargement de l'utilisateur pour Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+     response = requests.get(API_URL + f"get_user/{user_id}")
+     if response.status_code == 200:
+        user_data = response.json()
+        if user_data['id'] == 0:
+            return User(user_data['id'], user_data['pseudo'],True)
+        else:
+            return User(user_data['id'], user_data['pseudo'],False)
+
+
+@app.route('/page_admin')
+@login_required
+def admin_panel():
+    if current_user.is_admin:
+        return render_template('page_admin.html')
+    else:
+        return "page non autorisé"
+
+
 @app.route("/")
 def run():
     return render_template('index.html')
@@ -15,44 +51,41 @@ def run():
 def page_login():      
     return render_template('login.html')
 
-@app.before_request #cela pemert à la fonction dessous d'être appelé quand il y a une requête
-def avant_requete():
-    g.utilisateur = None
-    if "utilisateur" in session:
-        g.utilisateur = session["utilisateur"]
-        g.id = session["id"]
-
-@app.route("/supprime_session")
+@app.route('/supprime_session')
+@login_required
 def supprime_session():
-    session.pop("utilisateur", None)
-    session.pop("id", None)
-    flash("Vous êtes deconnecté","success")
-    return render_template("login.html")
+    logout_user()
+    flash("Vous êtes déconnecté", "success")
+    return render_template("index.html")
+
 
 @app.route("/home") #home page de l'application
+@login_required
 def home():
-    api_populaire = API_URL + "films_populaires"
-    if g.utilisateur:
+    api_populaire = API_URL + "films_populaires" #les films populaires sont les premiers films dans films.json
+    if current_user:
         response = requests.get(api_populaire)
         if response.status_code == 200:
             liste_films = []
             for film in response.json():
                 liste_films.append(film)
-            return render_template("home.html",utilisateur=session["utilisateur"],id=session["id"],films_populaires=liste_films)
+            return render_template("home.html",films_populaires=liste_films)
     return redirect(url_for("run"))
 
 @app.route('/description/<int:film_id>')
+@login_required
 def description_film(film_id):
     api_description = API_URL + f"trouver_film/{film_id}"
     response = requests.get(api_description)
     if response.status_code == 200:
         film = response.json()
         print("poster_path",film['poster_path'])
-        return render_template("description_film.html",utilisateur=session["utilisateur"],id=session["id"],film=film)
+        return render_template("description_film.html",film=film)
 
 @app.route('/videotheque/<int:id>')
-def get_videotheque(id):
-    api_videotheque = API_URL + f"ma_videotheque/{id}"
+@login_required
+def videotheque(id):
+    api_videotheque = API_URL + f"videotheque/{id}"
     reponse1 = requests.get(api_videotheque)
     liste_films = []
     if reponse1.status_code == 200:
@@ -67,9 +100,11 @@ def get_videotheque(id):
                 print("Film ajouté à la liste")
             else:
                 print("film introuvable")
-        return render_template("videotheque.html",utilisateur=session["utilisateur"],id=session["id"],films=liste_films) 
+        return render_template("videotheque.html",films=liste_films)  
+        
     else:
-      return "Echec de la requete"
+      return render_template("videotheque.html")
+      
 
 @app.route("/page_register")
 def page_register():
@@ -77,8 +112,6 @@ def page_register():
 
 @app.route("/register", methods=['POST'])
 def register():
-    session.pop("utilisateur",None)
-    session.pop("id",None)
     #recuperation des donnee de la requete post, c est en json.
     donnee = request.form
     #verification des champs, si ils sont vides
@@ -104,8 +137,6 @@ def register():
 
 @app.route("/verif_login", methods=['POST'])
 def verif_login():
-    session.pop("utilisateur",None)
-    session.pop("id",None)
     #recuperation des donnee de la requete post, c est en json.
     donnee = request.form
     print(donnee)
@@ -116,9 +147,13 @@ def verif_login():
             #recuperation un à un
             pseudo = donnee.get("username")
             password = donnee.get("password")
-            #cela supprime la session actuelle
-            session["utilisateur"] = pseudo
-            session["id"] = reponse.json().get("id")
+            user_id = reponse.json().get("id")
+            #creation du user
+            if user_id == 0:
+                user = User(user_id,pseudo,True)
+            else:
+                user = User(user_id,pseudo,False)
+            login_user(user)
             #url_for retourne sur la fonction qui s'appelle home, pas la route
             return redirect(url_for("home"))
         else:
@@ -135,10 +170,10 @@ def verif_login():
             return """
     
 @app.route("/supprimer_film/<int:film_id>")
+@login_required
 def supprimer_film(film_id):
     print("SUPPRIME LE FILM")
-    user_id = session["id"]
-    api_supprimer_film = API_URL + f"supprimer_film/{user_id}/{film_id}"
+    api_supprimer_film = API_URL + f"supprimer_film/{current_user.id}/{film_id}"
     reponse = requests.get(api_supprimer_film)
     if reponse.status_code == 200:
         flash("film supprimer",'success')
@@ -147,8 +182,9 @@ def supprimer_film(film_id):
 
 
 @app.route("/ajout_film/<int:film_id>")
+@login_required
 def ajout_film(film_id):
-    user_id = session["id"]
+    user_id = current_user.get_id()
     api_trouver_film = API_URL + f"ajout_film/{user_id}/{film_id}"
     reponse = requests.get(api_trouver_film)
     if reponse.status_code == 200:
@@ -165,6 +201,7 @@ def ajout_film(film_id):
     return redirect(url_for("home"))
 
 @app.route("/recherche_film", methods = ['POST'])
+@login_required
 def recherche_film():
     if request.method == 'POST':
         donne = request.form
@@ -176,12 +213,13 @@ def recherche_film():
         if reponse.status_code == 200:
             liste_films = reponse.json().get("liste_films")
             print("liste des films :",liste_films)
-            return render_template("recherche.html",utilisateur=session["utilisateur"],id=session["id"],films=liste_films)
+            return render_template("recherche.html",films=liste_films)
         else:
             flash("ereur probleme de connexion",'success')
-            return render_template("home.html",utilisateur=session["utilisateur"],id=session["id"],films=liste_films)
+            return render_template("home.html",films=liste_films)
         
 @app.route("/recherche_genre/<int:id>")
+@login_required
 def recherche_genre(id):
     api_recherche_genre = API_URL + f"recherche_genre/{id}"
     reponse = requests.get(api_recherche_genre)
@@ -189,32 +227,32 @@ def recherche_genre(id):
     if reponse.status_code == 200:
         liste_films = reponse.json().get("message")
         print("liste des films :",liste_films)
-        return render_template("recherche.html",utilisateur=session["utilisateur"],id=session["id"],films=liste_films)
+        return render_template("recherche.html",films=liste_films)
     else:
         print("erreur de communication")
-        return render_template("home.html",utilisateur=session["utilisateur"],id=session["id"],films=liste_films)
+        return render_template("home.html",films=liste_films)
 
 @app.route("/ajout_note",methods = ['POST'])
+@login_required
 def ajout_note():
     note = request.form.get('note')
     id_film = request.form.get('id')
-    user_id = request.form.get('user_id')
     """api_trouver_film = API_URL + f"trouver_film/{id_film}"
     reponse_film = requests.get(api_trouver_film)
     if reponse_film.status_code == 200:
         film_note = reponse_film.json()"""
     #return f"Note : {note} pour le film : {id_film} pour l'utilisateur : {user_id}"
-    api_note = API_URL + f"ajout_note/{user_id}/{id_film}/{note}"
+    api_note = API_URL + f"ajout_note/{current_user.id}/{id_film}/{note}"
     reponse = requests.get(api_note)
     if reponse.status_code == 200:
         message = reponse.json().get('message')
         film_note = reponse.json().get('film_note')
         if message == "Note ajoutée":
             flash("note ajouté","success")
-            return render_template("description_film.html",utilisateur=session["utilisateur"],id=session["id"],film=film_note)
+            return render_template("description_film.html",film=film_note)
         elif message == "Note modifiée":
             flash("Note modifié","success")
-            return render_template("description_film.html",utilisateur=session["utilisateur"],id=session["id"],film=film_note)
+            return render_template("description_film.html",film=film_note)
             #les messages flash ne fonctionne pas avec url_for, donc je suis
             #obligé de retrouver le film
             #return redirect(url_for('description_film',film_id=id_film))
@@ -222,7 +260,7 @@ def ajout_note():
         #return render_template("description_film.html",utilisateur=session["utilisateur"],id=session["id"],film=film_note)
     else:
         flash("probleme connexion, note non comptabilisé","success")
-        return render_template("description_film.html",utilisateur=session["utilisateur"],id=session["id"],film=film_note)
+        return render_template("description_film.html",film=film_note)
 
     
 if __name__ == "__main__":
